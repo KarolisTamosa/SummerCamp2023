f5b775cfa6d95fec936562b4d70c4a43f1cf939b
KarolisTamosa <ktamasi-ext@itracasa.es>
2023-07-18 12:41:31 +0200

    Commit inicial

---
+++ CalculadoraImpuestoDeCirculacionSolucion/CalculadoraImpuestoDeCirculacion/Coche.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraImpuestoDeCirculacion
+{
+    public class Coche
+    {
+        public enum Etiqueta
+        {
+            Sin_Etiqueta,
+            B,
+            C,
+            ECO,
+            CERO
+        }
+
+        private Etiqueta etiqueta;
+
+        public void fijarEtiqueta(Etiqueta etiqueta)
+        {
+            this.etiqueta = etiqueta;
+        }
+
+        public string Nombre { get; set; }
+        public double PrecioBase { get; set; }
+        public int AnoDelCoche { get; set; }
+
+        public double CalcularImpuestoDeCirculacion()
+        {
+            double valorBase = 0;
+
+            int antiguedad = DateTime.Now.Year - AnoDelCoche;
+            valorBase += PrecioBase * (antiguedad * 0.01);
+
+            switch (etiqueta)
+            {
+                case Etiqueta.Sin_Etiqueta:
+                    valorBase += PrecioBase * 0.25;
+                    break;
+                case Etiqueta.B:
+                    valorBase += PrecioBase * 0.15;
+                    break;
+                case Etiqueta.C:
+                    valorBase += PrecioBase * 0.10;
+                    break;
+                case Etiqueta.ECO:
+                    valorBase += PrecioBase * 0.05;
+                    break;
+            }
+            return valorBase;
+        }
+    }
+}
---
+++ CalculadoraImpuestoDeCirculacionSolucion/CalculadoraImpuestoDeCirculacion/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace CalculadoraImpuestoDeCirculacion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Audi = new Coche()
+            {
+                Nombre = "Audi",
+                PrecioBase = 1000,
+                AnoDelCoche = new DateTime(2023, 10, 09).Year,
+            };
+            Audi.fijarEtiqueta(Coche.Etiqueta.CERO);
+
+
+
+            var Peugeot = new Coche()
+            {
+                Nombre = "Peugeot",
+                PrecioBase = 1000,
+                AnoDelCoche = new DateTime(2022, 10, 09).Year
+            };
+            Peugeot.fijarEtiqueta(Coche.Etiqueta.ECO);
+
+
+
+            var Ford = new Coche()
+            {
+                Nombre = "Ford",
+                PrecioBase = 1000,
+                AnoDelCoche = new DateTime(1990, 10, 09).Year
+            };
+
+            var Subaru = new Coche()
+            {
+                Nombre = "Subaru",
+                PrecioBase = 1000,
+                AnoDelCoche = new DateTime(1875, 10, 09).Year
+            };
+            Subaru.fijarEtiqueta(Coche.Etiqueta.C);
+
+            var McLaren = new Coche()
+            {
+                Nombre = "McLaren",
+                PrecioBase = 1000,
+                AnoDelCoche = new DateTime(2007, 10, 09).Year
+            };
+            Subaru.fijarEtiqueta(Coche.Etiqueta.B);
+
+            List<Coche> listaVehiculos = new List<Coche>();
+            listaVehiculos.Add(Audi);
+            listaVehiculos.Add(Peugeot);
+            listaVehiculos.Add(Ford);
+            listaVehiculos.Add(Subaru);
+            listaVehiculos.Add(McLaren);
+
+            foreach (var vehiculo in listaVehiculos)
+            {
+                double impuesto = vehiculo.CalcularImpuestoDeCirculacion();
+                Console.WriteLine($"El precio total es: {vehiculo.Nombre} {vehiculo.PrecioBase + impuesto}");
+                Console.WriteLine($"El precio del impuesto es: {impuesto}");
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+    }
+}
