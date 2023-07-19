ed6af5876c0b30b0d43e71dfe94c4e9a23f91ece
KarolisTamosa <ktamasi-ext@itracasa.es>
2023-07-19 11:10:09 +0200

    Constructor de coches

---
+++ ConstructorDeCochesSolucion/ConstructorDeCoches/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConstructorDeCoches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? eleccion = string.Empty;
+            string marca = string.Empty;
+            string modelo = string.Empty;
+            string ano = string.Empty;
+            string color = string.Empty;
+
+            while (eleccion != "e" && eleccion != "E")
+            {
+                Console.Write("Pulsa (H) para crear tu coche ");
+                eleccion = Console.ReadLine();
+
+
+                switch (eleccion)
+                {
+                    case "H":
+                    case "h":
+                        var coche = new coche() { modelo = modelo, marca = marca, ano = ano, color = color };
+                        Console.Write("Escribe la marca de coche que quieres ");
+                        coche.marca = Console.ReadLine();
+                        Console.Write("Escribe el año de tu coche ");
+                        coche.ano = Console.ReadLine();
+                        Console.Write("Escribe el modelo de tu coche ");
+                        coche.modelo = Console.ReadLine();
+                        Console.Write("Escribe el color de tu coche ");
+                        coche.color = Console.ReadLine();
+                        Console.WriteLine($"la marca de tu coche es: { coche.marca} && el año es { coche.ano}" +
+                            $" && el modelo es { coche.modelo} && y el color es { coche.color}");
+                        break;
+
+
+                    case "e":
+                    case "E":
+                        break;
+
+
+                    default:
+                        Console.WriteLine("letra incorrecta");
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ ConstructorDeCochesSolucion/ConstructorDeCoches/coche.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDeCoches
+{
+    public class coche
+    {
+       public string? marca { get; set; }
+       public string? ano { get; set; }
+       public string? modelo { get; set; }
+       public string? color { get; set; }
+    }
+}
