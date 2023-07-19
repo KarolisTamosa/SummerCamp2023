a59352d05311d286ef6067feab2f435cfd3aeaae
KarolisTamosa <ktamasi-ext@itracasa.es>
2023-07-19 13:53:09 +0200

    Conversor de moneda

---
+++ ConversorDeMoneda/ConversorDeMoneda/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ConversorDeMoneda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? elecion = string.Empty;
+
+            while (elecion != "Q" && elecion != "q")
+            {
+                Console.WriteLine("******************************************************************************************************");
+                Console.WriteLine("Para cambiar de Euros a USD pulsa (E) ");
+                Console.WriteLine("Para cambiar de USD a euros palsa (U) ");
+                Console.WriteLine("Para cambiar de Euros a libras palsa (EL) ");
+                Console.WriteLine("Para cambiar de Libras a Euros palsa (L) ");
+                Console.WriteLine("para salir pulsa (Q) ");
+                Console.WriteLine("******************************************************************************************************");
+                elecion = Console.ReadLine();
+                double eurosAdolares, dolaresAEuros, euroALibras, LibraAEuros = 0;
+
+                switch (elecion)
+                {
+                    case "E":
+                    case "e":
+                        Console.Write("Escribe cual es la cantidad de Euros que quiere convertir ");
+                        eurosAdolares = conversor.EurosAdolares(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("******************************************************************************************************");
+                        Console.WriteLine($"La conversion de euros a dolares a fecha de /////({DateTime.Now})///// es de:");
+                        Console.WriteLine($"{eurosAdolares}");
+                        Console.WriteLine("******************************************************************************************************");
+                        break;
+
+                    case "U":
+                    case "u":
+                        Console.Write("Escribe cual es la cantidad de USD que quiere convertir ");
+                        dolaresAEuros = conversor.DolaresAEuros(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("******************************************************************************************************");
+                        Console.WriteLine($"La conversion de USD a euros a fecha de /////({DateTime.Now})///// es de:");
+                        Console.WriteLine($"{dolaresAEuros}");
+                        Console.WriteLine("******************************************************************************************************");
+                        break;
+                    case "EL":
+                    case "el":
+                        Console.Write("Escribe cual es la cantidad de Euros que quiere convertir ");
+                        euroALibras = conversor.EurosAlibras(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("******************************************************************************************************");
+                        Console.WriteLine($"La conversion de Euros a Libras a fecha de /////({DateTime.Now})///// es de:");
+                        Console.WriteLine($"{euroALibras}");
+                        Console.WriteLine("******************************************************************************************************");
+                        break;
+
+                    case "l":
+                    case "L":
+                        Console.Write("Escribe cual es la cantidad de Libras que quiere convertir ");
+                        LibraAEuros = conversor.LibrasAEuros(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("******************************************************************************************************");
+                        Console.WriteLine($"La conversion de Libras a Euros a fecha de /////({DateTime.Now})///// es de:");
+                        Console.WriteLine($"{LibraAEuros}");
+                        Console.WriteLine("******************************************************************************************************");
+                        break;
+
+                    case "q":
+                    case "Q":
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConversorDeMoneda/ConversorDeMoneda/conversor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoneda
+{
+    public class conversor
+    {
+        public static double EurosAdolares( string monedaeuros)
+        {
+            double euros = double.Parse(monedaeuros);
+            double eurosADolares = (euros * 1.12);
+            return eurosADolares;
+        }
+
+        public static double DolaresAEuros(string monedadolar)
+        {
+            double dolares = double.Parse(monedadolar);
+            double DolaresAEuros = (dolares * 0.89);
+            return DolaresAEuros;
+        }
+
+        public static double EurosAlibras(string monedaeuros)
+        {
+            double euros = double.Parse(monedaeuros);
+            double EurosAlibras = (euros * 0.87);
+            return EurosAlibras;
+        }
+
+        public static double LibrasAEuros(string monedalibra)
+        {
+            double libras = double.Parse(monedalibra);
+            double LibrasAEuros = (libras * 1.15);
+            return LibrasAEuros;
+        }
+    }
+}
