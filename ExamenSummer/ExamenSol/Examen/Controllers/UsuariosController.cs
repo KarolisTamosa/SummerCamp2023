20ff27c4010517fe8215398d07ac2f54ab3200b4
Karolis <ktamosaxlv@gmail.com>
2023-09-14 09:33:15 +0200

    Update creados DTOs, Controlador

---
+++ ExamenSummer/ExamenSol/Examen/Controllers/UsuariosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Repositorio;
+
+namespace Examen.Controllers
+{
+
+    [Route("api")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IRepositorioUsuario repositorioUsuarios;
+
+        public UsuariosController(IRepositorioUsuario repositorioUsuarios)
+        {
+            this.repositorioUsuarios = repositorioUsuarios;
+        }
+
+        [HttpGet]
+        [Route("usuarios")]
+        public async Task<ActionResult<List<Usuario>>> GetUsuarios()
+        {
+            var listaUsuarios = repositorioUsuarios.GetUsuarios();
+
+            if (listaUsuarios == null) { return NotFound(); }
+
+            return Ok(listaUsuarios);
+        }
+
+        [HttpPost]
+        [Route("registro")]
+        public async Task<IActionResult> PostUsuarios([FromBody] Usuario nuevoUsuario)
+        {
+            var userExists = await repositorioUsuarios.GetUsuario(nuevoUsuario.Nombre);
+
+            Usuario userAlta = new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = nuevoUsuario.Nombre,
+                Telefono = nuevoUsuario.Telefono,
+                FechaNacimiento = nuevoUsuario.FechaNacimiento
+            };
+
+            var usuario = await repositorioUsuarios.PostUsuario(userAlta);
+
+            return Ok(nuevoUsuario);
+        }
+    }
+}
+
---
+++ ExamenSummer/ExamenSol/Models/UsuarioAltaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class UsuarioAltaDto
+    {
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
