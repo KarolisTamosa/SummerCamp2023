e57a87cb5eb12609395d62261894f3fd91d1e2c4
Karolis <ktamosaxlv@gmail.com>
2023-09-14 12:32:53 +0200

    gitignore

--- ExamenSummer/ExamenSol/Examen/Program.cs
+++ ExamenSummer/ExamenSol/Examen/Program.cs
@@ -23,6 +23,16 @@ namespace Examen
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
 
             app.UseCors("AllowAll");
