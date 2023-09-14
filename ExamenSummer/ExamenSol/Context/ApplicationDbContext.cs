20ceb765a42693d6a323199ff0740d33e1934242
Karolis <ktamosaxlv@gmail.com>
2023-09-14 09:09:23 +0200

    Context y repo

---
+++ ExamenSummer/ExamenSol/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System.Collections.Generic;
+
+namespace Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
--- ExamenSummer/ExamenSol/Repositorio/RepositorioUsuario.cs
+++ ExamenSummer/ExamenSol/Repositorio/RepositorioUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace Repositorio
 {
-    public class RepositorioUsuario
+    public class RepositorioUsuario : IRepositorioUsuario
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(User nuevoUsuario)
+        {
+            try
+            {
+                _context.Users.Add(nuevoUsuario);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                // Manejar la excepción según tus necesidades
+                throw new Exception("Error al agregar un usuario.", ex);
+            }
+        }
+
+        public IEnumerable<user> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public user GetUserById(string id)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        // Otros métodos de repositorio según tus necesidades
     }
+}
 }
